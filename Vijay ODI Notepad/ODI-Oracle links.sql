0c0fd904ff30e4f45072c85170bea0b0c60db880
vijay85p <160473365+vijay85p@users.noreply.github.com>
2024-02-19 14:18:52 +0530

    Add files via upload

---
+++ Vijay ODI Notepad/ODI-Oracle links.sql
@@ -0,0 +1,89 @@
+How do I connect to a user in PDB?
+https://www.thegeekdiary.com/connecting-pdb-in-oracle-12c-and-19c/
+https://www.thegeekdiary.com/how-to-connect-to-an-oracle-pluggable-database-pdb/
+https://ohiocomputeracademy.com/database/unlocking-scott-schema-in-oracle/#:~:text=Modifying%20scott.&text=If%20you%20have%20followed%20the,%E2%80%9CCONNECT%20SCOTT%2FTIGER%E2%80%9D.&text=Now%20copy%20and%20paste%20CONNECT,CONNECT%20SCOTT%2FTIGER%40orclpdb.
+
+
+YouTube Links
+=============
+
+https://www.youtube.com/watch?v=GzFEyzOAlh8
+https://www.youtube.com/watch?v=U-NQlwbgZLo
+https://www.youtube.com/@TRRaveendra/playlists(Tech Lake Videos)
+https://www.youtube.com/watch?v=q6ymMsSbqcU(Table to File )
+
+ODI Links
+=========
+
+https://odielt.wordpress.com/2016/04/11/how-to-load-data-into-multiple-targets-using-single-interface-in-odi/
+https://www.appsloveworld.com/oracle/100/220/odi-11g-interface-sql-data-to-multiple-flat-files
+https://odielt.wordpress.com/2016/04/16/odi-architecture-and-components/
+http://maxwellhu.blogspot.com/2013/04/how-to-export-data-to-flat-file-with-odi.html(Table to file)
+
+https://odielt.wordpress.com/2016/11/12/how-to-load-data-from-file-to-table-using-odi/
+http://oditraining.blogspot.com/2014/10/oracle-subqueries-with-examples.html(SubQuery)
+https://www.clearpeaks.com/oracle-data-integrator-12c-subquery-filter-component/(SubQuery)
+https://sonra.io/apache/using-apache-airflow-to-build-a-data-pipeline-on-aws/(SubQuery)
+https://bigdataenthusiast.wordpress.com/2020/03/29/odi12c-setting-cdc-journalizing/(CDC )
+https://docs.oracle.com/cd/E17904_01/integrate.1111/e12643/data_capture.htm#ODIDG274(CDC Changes)
+https://www.kpipartners.com/blog/bid/155022/A-Detailed-Explanation-of-ODI-Knowledge-Modules-JKM-SKM
+https://samudrabandaranayake.wordpress.com/category/odi/
+https://www.learn-it-with-examples.com/datawarehouse-bigdata/data-integration/odi
+/install-oracle-odi-11g-windows.html(ODI installation)
+
+https://www.youtube.com/watch?v=l13AIvhYc-Y(Master and Work Rep Creation)
+https://youtu.be/l13AIvhYc-Y(Master and Work Rep Creation)
+
+https://www.hugedomains.com/domain_profile.cfm?d=odipundits.com
+https://forums.oracle.com/ords/apexds/post/odi12c-monitoring-without-odi-console-5707
+
+https://bigdataenthusiast.wordpress.com/2017/11/26/dataserver-physical-schema-logical-schema
+-context-oracle-data-integrator-11g/(LogicalSchema,Context & Physical Schema)
+
+https://bigdataenthusiast.wordpress.com/learn-oracle-data-integrator/(ODI Bigdata)
+https://odielt.wordpress.com/2016/06/04/how-to-implement-group-by-and-having-clause-in-odi/
+
+https://odiexperts.com/11g-oracle-data-integrator-part-611g-union-minus-intersect/(SET OPERATOR)
+
+ODI REF Links
+=============
+
+http://odiexperts.com/context-logical-and-physical-schema-how-does-it-work/(Data Server,PS,LS)
+
+https://datacadamia.com/doc/odi/webhelp/en/ref_api/getobjectname.html(I$,C$,E$ tables)
+https://jomavifi.wordpress.com/(To check the SCD Behaviour)
+https://aravinddarla.blogspot.com/search/label/ODI?&max-results=100(ODI KM)
+http://odiexperts.com/how-to-use-plsql-procedures-and-functions-in-odi/(ODI PROCEDURE)
+http://odiexperts.com/generate-multiple-scenarios/#comment-10241(Generate Scenario)
+http://odiexperts.com/11g-oracle-data-integrator-part-811g-table-partitioning-supported/(Partitions)
+https://odielt.wordpress.com/2016/04/08/com-sunopsis-tools-core-exception-snp
+ssimplemessageexception-odi-17517-error-during-task-interpretation/#:~:text=Snp
+sSimpleMessageException%3A%20ODI%2D17517%3A%20Error%20during%20task%20interpretation.,
+-Task%3A%201&text=This%20error%20occurs%20in%20the,schema%20name%20as%20it%20is.(odi ref FUNCTION)
+
+https://odielt.wordpress.com/2016/04/11/how-to-load-data-into-multiple-targets-using-single-interface-in-odi/(MULTIPLE FILES)
+https://odielt.wordpress.com/2016/04/11/how-to-load-data-into-multiple-targets-using-single-interface-in-odi/
+http://odiexperts.com/processing-multiple-interface-through-single-package/
+
+https://dokumen.tips/data-analytics/odi-11g-multiple-flat-files-to-oracle-db-table-by-taking-file-name-dynamically.html?page=1
+https://dokumen.tips/data-analytics/odi-11g-multiple-flat-files-to-oracle-db-table-by-taking-file-name-dynamically.html?page=9
+
+
+https://datacadamia.com/doc/odi/webhelp/en/ref_api/getobjectname.html
+https://www.tutorialspoint.com/obiee/obiee_schema.htm
+
+
+
+ORacle Links
+============
+
+https://tipsfororacle.blogspot.com/2017/04/handle-exceptions-inside-cursor-loop.html
+https://www.oracletutorial.com/oracle-analytic-functions/oracle-lag/
+https://oracle-base.com/articles/misc/rank-dense-rank-first-last-analytic-functions
+
+https://rajeevjhaoracle.wordpress.com/2013/05/28/how-to-check-if-your-oracle-client-is-32-bit-or-64-bit/
+https://mkyong.com/oracle/how-to-uninstall-oracle-database-19c-on-windows/
+https://www.youtube.com/watch?v=Ap4qLSRaM7o
+https://alekciss.com/install-oracle-database-19c-on-windows/
+https://dotnettutorials.net/lesson/how-to-download-and-install-oracle-19c-softwareon-windows-10/
+https://www.tutorialspoint.com/plsql/plsql_exceptions.htm
